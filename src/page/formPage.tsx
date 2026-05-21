5599a73bf7b8302ed4e0103febfa1d5aff3a6cbe
navi-lee <2625417961@qq.com>
2026-05-21 23:40:23 +0800

    add:开始写表单页

--- src/page/formPage.tsx
+++ src/page/formPage.tsx
@@ -21,208 +21,50 @@ interface FormValues {
   agreement: boolean;
 }
 
-const FormPage = () => {
-  const [form] = Form.useForm<FormValues>();
-
-  // 自定义校验规则
-  const validatePhone = (_: any, value: string) => {
+const FormPage:React.FC = ()=>{
+  const [form] = Form.useForm<FormValues>()
+  const validatePhone = (_:any, value:string)=>{
     const phoneRegex = /^1[3-9]\d{9}$/;
-    if (!value) {
-      return Promise.reject(new Error('请输入手机号'));
+    if(!value){
+      return Promise.reject(new Error('需要输入手机号'))
     }
-    if (!phoneRegex.test(value)) {
-      return Promise.reject(new Error('请输入正确的11位手机号'));
+    if(!phoneRegex.test(value)){
+      return Promise.reject(new Error('请输入正确的十一位手机号'))
     }
-    return Promise.resolve();
-  };
-
-  const validateUsername = (_: any, value: string) => {
-    if (!value) {
-      return Promise.reject(new Error('请输入用户名'));
+    return Promise.resolve()
+  }
+  const validateUserName=(_:any, name:string)=>{
+    if(!name){
+      return Promise.reject(new Error('请输入用户名'))
     }
-    if (value.length < 3) {
-      return Promise.reject(new Error('用户名长度至少为3个字符'));
+    if(name.length<3){
+      return Promise.reject(new Error('用户名至少三个字符'))
+
     }
-    return Promise.resolve();
-  };
-
-
-
-  const onFinish = (values: FormValues) => {
-    console.log('表单数据:', values);
-    message.success('表单提交成功！');
-    form.resetFields();
-  };
-
-  const onFinishFailed = (errorInfo: any) => {
-    console.log('表单错误:', errorInfo);
-    message.error('表单提交失败，请检查错误信息');
-  };
-
+    return Promise.resolve()
+  }
   return (
     <div style={{ maxWidth: 800, margin: '0 auto', padding: 20 }}>
-      <Title level={2}>用户信息表单</Title>
-      <Paragraph type="secondary">
-        请填写完整的个人信息，带 * 为必填项
-      </Paragraph>
-
+      <Title level={2}>表单页</Title>
+      <Typography.Paragraph>请填写完整个人信息，其中带星号的是必填项。</Typography.Paragraph>
       <Card>
-        <Form
-            form={form}
-            layout='vertical'
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-
-          initialValues={{
-            gender: 'male',
-            age: 20,
-            education: 'bachelor',
-            skills: [],
-            agreement: false,
-          }}
+      <Form
+        form={form}
+        layout='vertical'
+      >
+       <Divider orientation='center'>基本信息</Divider>
+        <Form.Item
+        name='username'
+        label='用户名'
+        rules={[{validator :validatePhone}]}
         >
-          <Divider orientation="left">基本信息</Divider>
-          
-          <Space direction="vertical" style={{ width: '100%' }}>
-            <Form.Item
-              name="username"
-              label="用户名"
-              rules={[{ validator: validateUsername }]}
-            >
-              <Input prefix={<UserOutlined />} placeholder="请输入用户名" />
-            </Form.Item>
-
-            <Form.Item
-              name="email"
-              label="邮箱"
-              rules={[
-                { required: true, message: '请输入邮箱' },
-                { type: 'email', message: '请输入正确的邮箱格式' },
-              ]}
-            >
-              <Input prefix={<MailOutlined />} placeholder="请输入邮箱" />
-            </Form.Item>
-
-            <Form.Item
-              name="phone"
-              label="手机号"
-              rules={[{ validator: validatePhone }]}
-            >
-              <Input prefix={<PhoneOutlined />} placeholder="请输入手机号" />
-            </Form.Item>
-
-            <Form.Item
-              name="gender"
-              label="性别"
-              rules={[{ required: true, message: '请选择性别' }]}
-            >
-              <Radio.Group>
-                <Radio value="male">男</Radio>
-                <Radio value="female">女</Radio>
-                <Radio value="other">其他</Radio>
-              </Radio.Group>
-            </Form.Item>
-
-            <Form.Item
-              name="age"
-              label="年龄"
-              rules={[
-                { required: true, message: '请输入年龄' },
-                { type: 'number', min: 18, max: 120, message: '年龄必须在18-120之间' },
-              ]}
-            >
-              <InputNumber min={1} max={120} style={{ width: '100%' }} />
-            </Form.Item>
-          </Space>
-
-          <Divider orientation="left">教育与技能</Divider>
-          <Space direction="vertical" style={{ width: '100%' }}>
-            <Form.Item
-              name="education"
-              label="最高学历"
-              rules={[{ required: true, message: '请选择学历' }]}
-            >
-              <Select placeholder="请选择最高学历">
-                <Option value="highschool">高中</Option>
-                <Option value="associate">专科</Option>
-                <Option value="bachelor">本科</Option>
-                <Option value="master">硕士</Option>
-                <Option value="doctor">博士</Option>
-              </Select>
-            </Form.Item>
-
-            <Form.Item
-              name="skills"
-              label="技能"
-              rules={[{ required: true, message: '请至少选择一项技能' }]}
-            >
-              <Select mode="multiple" placeholder="请选择技能">
-                <Option value="javascript">JavaScript</Option>
-                <Option value="typescript">TypeScript</Option>
-                <Option value="react">React</Option>
-                <Option value="vue">Vue</Option>
-                <Option value="node">Node.js</Option>
-                <Option value="python">Python</Option>
-              </Select>
-            </Form.Item>
-
-            <Form.Item
-              name="startDate"
-              label="预计工作日期"
-              rules={[{ required: true, message: '请选择日期范围' }]}
-            >
-              <RangePicker placeholder={['开始日期', '结束日期']} />
-            </Form.Item>
-          </Space>
-
-          <Divider orientation="left">其他信息</Divider>
-          <Space direction="vertical" style={{ width: '100%' }}>
-            <Form.Item
-              name="bio"
-              label="个人简介"
-              rules={[
-                { required: true, message: '请输入个人简介' },
-                { min: 10, message: '个人简介至少10个字符' },
-              ]}
-            >
-              <TextArea 
-                rows={2} 
-                placeholder="请输入个人简介"
-              />
-            </Form.Item>
-
-
-
-            <Form.Item
-              name="agreement"
-              valuePropName="checked"
-              rules={[
-                {
-                  validator: (_, value) =>
-                    value ? Promise.resolve() : Promise.reject(new Error('必须同意用户协议')),
-                },
-              ]}
-            >
-              <Checkbox>
-                我已阅读并同意 <a href="#">用户协议</a> 和 <a href="#">隐私政策</a>
-              </Checkbox>
-            </Form.Item>
-
-            <Form.Item>
-              <Space style={{ justifyContent: 'center' }}>
-                <Button type="primary" htmlType="submit" size="large">
-                  提交表单
-                </Button>
-                <Button onClick={() => form.resetFields()} size="large">
-                  重置
-                </Button>
-              </Space>
-            </Form.Item>
-          </Space>
-        </Form>
+          <Input placeholder='请输入用户名'></Input>
+        </Form.Item>
+        
+      </Form>
       </Card>
     </div>
-  );
-};
+  )
+}
 
-export default FormPage;
+export default FormPage
